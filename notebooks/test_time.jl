ae8657be9059294d3c30f50439cd4cbd6ca90852
jaimerz <jaimerz011235813@gmail.com>
2023-05-23 09:19:15 +0100

    time

--- notebooks/test_time.jl
+++ notebooks/test_time.jl
@@ -1,96 +1,97 @@
-println(Threads.nthreads())
-
 using LinearAlgebra
-using Turing
 using LimberJack
+using CSV
+using YAML
+using BenchmarkTools
 using PythonCall
-using NPZ
-using BenchmarkTools;
+using ForwardDiff
+sacc = pyimport("sacc");
 
-np = pyimport("numpy");
+#println("My id is ", myid(), " and I have ", Threads.nthreads(), " threads")
 
-meta = np.load("../data/DESY1/gcgc_gcwl_wlwl_meta.npz")
-tracers_names = pyconvert(Vector{String}, meta["tracers"])
-pairs = pyconvert(Vector{Vector{String}}, meta["pairs"])
-pairs_ids = pyconvert(Vector{Vector{Int}}, meta["pairs_ids"])
-idx = pyconvert(Vector{Int}, meta["idx"])
-data_vector = pyconvert(Vector{Float64}, meta["cls"])
-cov_tot = pyconvert(Matrix{Float64}, meta["cov"])
-inv_cov_tot = pyconvert(Matrix{Float64}, meta["inv_cov"]);
+sacc_path = "../../data/FD/cls_FD_covG.fits"
+yaml_path = "../../data/DESY1/gcgc_gcwl_wlwl.yml"
+nz_path = "../../data/DESY1/nzs"
+sacc_file = sacc.Sacc().load_fits(sacc_path)
+yaml_file = YAML.load_file(yaml_path)
+#nz_DESwl__0 = npzread(string(nz_path, "nz_DESwl__0.npz"))
+#nz_DESwl__1 = npzread(string(nz_path, "nz_DESwl__1.npz"))
+#nz_DESwl__2 = npzread(string(nz_path, "nz_DESwl__2.npz"))
+#nz_DESwl__3 = npzread(string(nz_path, "nz_DESwl__3.npz"))
+meta, files = make_data(sacc_file, yaml_file)
+                        #nz_DESwl__0=nz_DESwl__0,
+                        #nz_DESwl__1=nz_DESwl__1,
+                        #nz_DESwl__2=nz_DESwl__2,
+                        #nz_DESwl__3=nz_DESwl__3)
 
-files = npzread("../data/DESY1/gcgc_gcwl_wlwl_files.npz");
+data = meta.data
+cov = meta.cov
 
-#path = "/home/jaime/PhD/LimberJack.jl/chains/carlos_chains/cl_cross_corr_v3_DES_K1000_all_mag_correctMag/"
-#pars = np.loadtxt(string(path, "cl_cross_corr_v3_DES_K1000_all_mag_correctMag.bestfit"))
-#pars = pyconvert(Vector{Float64}, pars);
+p = [ 2.80864713e-01,  3.98941817e-02,  8.03805162e-01,  8.15499178e-01,
+9.67896094e-01,  1.45433358e+00,  1.77905433e+00,  1.70409115e+00,
+2.08994246e+00,  2.27404915e+00,  2.48932691e-03,  5.69699152e-03,
+-5.19871207e-03,  4.53661141e-03, -9.45028216e-03,  5.72751378e-03,
+-4.12309149e-02,  1.28279270e-03, -3.74374413e-02,  2.53967236e-02,
+1.90603567e-02,  2.52821784e-02,  2.57635754e-02,  2.73739640e-01,
+-6.99330759e-01]
 
-#cls_carlos_ND = np.load(string(path, "cl_cross_corr_bestfit_info.npz"))
-#cls_carlos_SD = np.load(string(path, "cl_cross_corr_bestfit_info_copy.npz"))
-#cls_carlos_FD = np.append(cls_carlos_SD["cls"], cls_carlos_ND["cls"])
-#cls_carlos_FD = pyconvert(Vector{Float64}, cls_carlos_FD)
-#carlos_chi = pyconvert(Float64, - (cls_carlos_ND["chi2"] +  cls_carlos_SD["chi2"]));
+function make_cls(p)
+    #KiDS priors
+    Ωm = p[1]
+    Ωb = p[2]
+    h = p[3]
+    σ8 = p[4]
+    ns = p[5]
 
-Ωm = 0.30 #pars[1] + pars[2]
-s8 = 0.81 #pars[46]
-function make_cls(; Ωm=Ωm, s8=s8)
-    #nuisances = Dict("DESgc__0_0_b" => pars[6],
-    #                 "DESgc__1_0_b" => pars[7],
-    #                 "DESgc__2_0_b" => pars[8],
-    #                 "DESgc__3_0_b" => pars[9],
-    #                 "DESgc__4_0_b" => pars[10],
-    #                 "DESgc__0_0_dz" => pars[11],
-    #                 "DESgc__1_0_dz" => pars[12],
-    #                 "DESgc__2_0_dz" => pars[13],
-    #                 "DESgc__3_0_dz" => pars[14],
-    #                 "DESgc__4_0_dz" => pars[15],
-    #    
-    #                 "A_IA" => pars[16],
-    #                 "alpha_IA" => pars[17],
-    #    
-    #                 "DESwl__0_e_m" => pars[18],
-    #                 "DESwl__1_e_m" => pars[19],
-    #                 "DESwl__2_e_m" => pars[20],
-    #                 "DESwl__3_e_m" => pars[21],
-    #                 "DESwl__0_e_dz" => pars[22],
-    #                 "DESwl__1_e_dz" => pars[23],
-    #                 "DESwl__2_e_dz" => pars[24],
-    #                 "DESwl__3_e_dz" => pars[25],
-    #    
-    #                 "eBOSS__0_0_b" => pars[26],
-    #                 "eBOSS__1_0_b" => pars[27],
-    #
-    #                 "DECALS__0_0_b" => pars[28],
-    #                 "DECALS__1_0_b" => pars[29],
-    #                 "DECALS__2_0_b" => pars[30],
-    #                 "DECALS__3_0_b" => pars[31],
-    #                 "DECALS__0_0_dz" => pars[32],
-    #                 "DECALS__1_0_dz" => pars[33],
-    #                 "DECALS__2_0_dz" => pars[34],
-    #                 "DECALS__3_0_dz" => pars[35],
-    #                
-    #                 "KiDS1000__0_e_m" => pars[36],
-    #                 "KiDS1000__1_e_m" => pars[37],
-    #                 "KiDS1000__2_e_m" => pars[38],
-    #                 "KiDS1000__3_e_m" => pars[39],
-    #                 "KiDS1000__4_e_m" => pars[40],
-    #                 "KiDS1000__0_e_dz" => pars[41],
-    #                 "KiDS1000__1_e_dz" => pars[42],
-    #                 "KiDS1000__2_e_dz" => pars[43],
-    #                 "KiDS1000__3_e_dz" => pars[44],
-    #                 "KiDS1000__4_e_dz" => pars[45])
+    DESgc__0_b = p[6]
+    DESgc__1_b = p[7]
+    DESgc__2_b = p[8]
+    DESgc__3_b = p[9]
+    DESgc__4_b = p[10]
+    DESgc__0_dz = p[11]
+    DESgc__1_dz = p[12]
+    DESgc__2_dz = p[13]
+    DESgc__3_dz = p[15]
+    DESgc__4_dz = p[16]
+    DESwl__0_dz = p[17]
+    DESwl__1_dz = p[18]
+    DESwl__2_dz = p[19]
+    DESwl__3_dz = p[20]
+    DESwl__0_m = p[21]
+    DESwl__1_m = p[22]
+    DESwl__2_m = p[23]
+    DESwl__3_m = p[24]
+    A_IA = p[25]
+    alpha_IA = p[26]
 
-    Ωb = 0.05 #pars[1]
-    h = 0.67 #pars[5]
-    ns = 0.96 #pars[4]
-    
-    cosmology = LimberJack.Cosmology(Ωm, Ωb, h, ns, s8, 
-                                     tk_mode="emulator",
-                                     Pk_mode="Halofit")
+    nuisances = Dict("DESgc__0_b" => DESgc__0_b,
+                     "DESgc__1_b" => DESgc__1_b,
+                     "DESgc__2_b" => DESgc__2_b,
+                     "DESgc__3_b" => DESgc__3_b,
+                     "DESgc__4_b" => DESgc__4_b,
+                     "DESgc__0_dz" => DESgc__0_dz,
+                     "DESgc__1_dz" => DESgc__1_dz,
+                     "DESgc__2_dz" => DESgc__2_dz,
+                     "DESgc__3_dz" => DESgc__3_dz,
+                     "DESgc__4_dz" => DESgc__4_dz,
+                     "DESwl__0_dz" => DESwl__0_dz,
+                     "DESwl__1_dz" => DESwl__1_dz,
+                     "DESwl__2_dz" => DESwl__2_dz,
+                     "DESwl__3_dz" => DESwl__3_dz,
+                     "DESwl__0_m" => DESwl__0_m,
+                     "DESwl__1_m" => DESwl__1_m,
+                     "DESwl__2_m" => DESwl__2_m,
+                     "DESwl__3_m" => DESwl__3_m,
+                     "A_IA" => A_IA,
+                     "alpha_IA" => alpha_IA,)
 
-    return Theory(cosmology,
-                  tracers_names, pairs,
-                  idx, files)
+    cosmology = Cosmology(Ωm, Ωb, h, ns, σ8;
+                          tk_mode="EisHu",
+                          Pk_mode="Halofit")
 
+    return Theory(cosmology, meta, files; Nuisances=nuisances)
 end
 
-println(@benchmark make_cls())
+@benchmark make_cls(p)
+
+@benchmark ForwardDiff.gradient(make_cls, p)
