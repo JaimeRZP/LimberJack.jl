bbcc6c1ad202dbbf18b8b3e0193b9cdbecce8b8b
jaimerz <jaimerz011235813@gmail.com>
2023-05-31 13:57:38 +0100

    compat helper

--- test/runtests.jl
+++ test/runtests.jl
@@ -17,15 +17,13 @@ test_output = Dict{String}{Vector}()
 cosmo_EisHu = Cosmology(nk=300, nz=300, nz_pk=70, tk_mode="EisHu")
 cosmo_emul = Cosmology(Ωm=(0.12+0.022)/0.75^2, Ωb=0.022/0.75^2, h=0.75, ns=1.0, σ8=0.81,
                        nk=300, nz=300, nz_pk=70, tk_mode="emupk")
-
+cosmo_emul_As = Cosmology(Ωm=0.27, Ωb=0.046, h=0.7, ns=1.0, As=2.097e-9,
+                       nk=300, nz=300, nz_pk=70, tk_mode="emupk")
 cosmo_EisHu_nonlin = Cosmology(nk=300, nz=300, nz_pk=70,
                                tk_mode="EisHu", Pk_mode="Halofit")
 cosmo_emul_nonlin = Cosmology(Ωm=(0.12+0.022)/0.75^2, Ωb=0.022/0.75^2, h=0.75, ns=1.0, σ8=0.81,
                               nk=300, nz=300, nz_pk=70,
                               tk_mode="emupk", Pk_mode="Halofit")
-cosmo_Bolt_nonlin = Cosmology(Ωm=0.27, Ωb=0.046, h=0.70, ns=1.0, σ8=0.81,
-                              nk=70, nz=300, nz_pk=70,
-                              tk_mode="Bolt", Pk_mode="Halofit")
 
 @testset "All tests" begin
     @testset "CreateCosmo" begin
@@ -76,39 +74,25 @@ cosmo_Bolt_nonlin = Cosmology(Ωm=0.27, Ωb=0.046, h=0.70, ns=1.0, σ8=0.81,
         @test all(@. (abs(pk_emul/pk_emul_bm-1.0) <  0.05))
     end
 
-    if extensive
-        @testset "linear_Pk_As" begin
-            cosmo_emul_As = Cosmology(Ωm=0.27, Ωb=0.046, h=0.7, ns=1.0, As=2.097e-9,
-                                    nk=300, nz=300, nz_pk=70, tk_mode="emupk")
-            cosmo_Bolt_As = Cosmology(Ωm=0.27, Ωb=0.046, h=0.7, ns=1.0, As=2.097e-9,
-                                     nk=70, nz=300, nz_pk=70, tk_mode="Bolt")
-            ks = exp.(LimberJack.emulator.training_karr)
-            pk_emul = nonlin_Pk(cosmo_emul_As, ks, 0.0)
-            pk_Bolt = nonlin_Pk(cosmo_Bolt_As, ks, 0.0)
-            pk_emul_bm = test_results["pk_emul_As"]
-            pk_Bolt_bm = test_results["pk_Bolt_As"]
-            merge!(test_output, Dict("pk_emul_As"=> pk_emul))
-            merge!(test_output, Dict("pk_Bolt_As"=> pk_Bolt))
-            @test all(@. (abs(pk_emul/pk_emul_bm-1.0) <  0.05))
-            @test all(@. (abs(pk_Bolt/pk_Bolt_bm-1.0) <  0.05))
-        end
-    end                 
+    @testset "linear_Pk_As" begin
+        ks = exp.(LimberJack.emulator.training_karr)
+        pk_emul = nonlin_Pk(cosmo_emul_As, ks, 0.0)
+        pk_emul_bm = test_results["pk_emul_As"]
+        merge!(test_output, Dict("pk_emul_As"=> pk_emul))
+        @test all(@. (abs(pk_emul/pk_emul_bm-1.0) <  0.05))
+    end             
 
     @testset "nonlinear_Pk" begin
         ks = exp.(LimberJack.emulator.training_karr)
         pk_EisHu = nonlin_Pk(cosmo_EisHu_nonlin, ks, 0)
         pk_emul = nonlin_Pk(cosmo_emul_nonlin, ks, 0)
-        pk_Bolt = nonlin_Pk(cosmo_Bolt_nonlin, ks, 0)
         pk_EisHu_bm = test_results["pk_EisHu_nonlin"]
         pk_emul_bm = test_results["pk_emul_nonlin"]
-        pk_Bolt_bm = test_results["pk_Bolt_nonlin"]
         merge!(test_output, Dict("pk_EisHu_nonlin"=> pk_EisHu))
         merge!(test_output, Dict("pk_emul_nonlin"=> pk_emul))
-        merge!(test_output, Dict("pk_Bolt_nonlin"=> pk_Bolt))
         # It'd be best if this was < 1E-4...
         @test all(@. (abs(pk_EisHu/pk_EisHu_bm-1.0) < 0.005))
         @test all(@. (abs(pk_emul/pk_emul_bm-1.0) < 0.05))
-        @test all(@. (abs(pk_Bolt/pk_Bolt_bm-1.0) < 0.05))
         
     end
    
@@ -362,33 +346,7 @@ cosmo_Bolt_nonlin = Cosmology(Ωm=0.27, Ωb=0.046, h=0.70, ns=1.0, σ8=0.81,
         # crosses zero
         @test median(lin_EisHu_autodiff./lin_EisHu_num.-1) < 0.05
         @test median(lin_emul_autodiff./lin_emul_num.-1) < 0.05
-    end
-
-    if false #extensive
-        @testset "IsBoltPkDiff" begin
-            logk = range(log(0.0001), stop=log(100.0), length=20)
-            ks = exp.(logk)
-
-            function lin_Bolt(p)
-                cosmo = Cosmology(Ωm=p, tk_mode="Bolt", Pk_mode="linear")
-                pk = lin_Pk(cosmo, ks, 0.)
-                return pk
-            end
-
-            Ωm0 = 0.25
-            dΩm = 0.01
-
-            lin_Bolt_autodiff = abs.(ForwardDiff.derivative(lin_Bolt, Ωm0))
-            lin_Bolt_num = abs.((lin_Bolt(Ωm0+dΩm)-lin_Bolt(Ωm0-dΩm))/(2dΩm))
-
-            merge!(test_output, Dict("lin_Bolt_autodiff"=> lin_Bolt_autodiff))
-            merge!(test_output, Dict("lin_Bolt_num"=> lin_Bolt_num))        
-            # Median needed since errors shoot up when derivatieve
-            # crosses zero
-            @test median(lin_Bolt_autodiff./lin_Bolt_num.-1) < 0.05
-        end
     end    
-    
 
     @testset "IsNonlinPkDiff" begin
         if extensive
@@ -397,7 +355,6 @@ cosmo_Bolt_nonlin = Cosmology(Ωm=0.27, Ωb=0.046, h=0.70, ns=1.0, σ8=0.81,
         else
             ks = exp.(LimberJack.emulator.training_karr)
         end
-
                                                 
         function nonlin_EisHu(p)
             cosmo = Cosmology(Ωm=p, tk_mode="EisHu", Pk_mode="Halofit")
@@ -405,44 +362,16 @@ cosmo_Bolt_nonlin = Cosmology(Ωm=0.27, Ωb=0.046, h=0.70, ns=1.0, σ8=0.81,
             return pk
         end
 
-        #=
-        function nonlin_emul(p)
-            cosmo = Cosmology(Ωm=p, tk_mode="emupk", Pk_mode="Halofit")
-            pk = nonlin_Pk(cosmo, ks, 0.)
-            return pk
-        end
-
-        function nonlin_Bolt(p)
-            cosmo = Cosmology(Ωm=p, tk_mode="Bolt", Pk_mode="Halofit")
-            pk = nonlin_Pk(cosmo, ks, 0.)
-            return pk
-        end
-        =#
-
         Ωm0 = 0.25
         dΩm = 0.01
-
         nonlin_EisHu_autodiff = abs.(ForwardDiff.derivative(nonlin_EisHu, Ωm0))
-        #nonlin_emul_autodiff = abs.(ForwardDiff.derivative(nonlin_emul, Ωm0))
-        #nonlin_Bolt_autodiff = abs.(ForwardDiff.derivative(nonlin_Bolt, Ωm0))
-        nonlin_EisHu_num = abs.((nonlin_EisHu(Ωm0+dΩm)-nonlin_EisHu(Ωm0-dΩm))/(2dΩm))
-        #nonlin_emul_num = abs.((nonlin_emul(Ωm0+dΩm)-nonlin_emul(Ωm0-dΩm))/(2dΩm));
-        #nonlin_Bolt_num = abs.((nonlin_Bolt(Ωm0+dΩm)-nonlin_Bolt(Ωm0-dΩm))/(2dΩm));
-                                                
+        nonlin_EisHu_num = abs.((nonlin_EisHu(Ωm0+dΩm)-nonlin_EisHu(Ωm0-dΩm))/(2dΩm))                               
         merge!(test_output, Dict("nonlin_EisHu_autodiff"=> nonlin_EisHu_autodiff))
-        #merge!(test_output, Dict("nonlin_emul_autodiff"=> nonlin_emul_autodiff))
-        #merge!(test_output, Dict("nonlin_Bolt_autodiff"=> nonlin_Bolt_autodiff))
         merge!(test_output, Dict("nonlin_EisHu_num"=> nonlin_EisHu_num))
-        #merge!(test_output, Dict("nonlin_emul_num"=> nonlin_emul_num))
-        #merge!(test_output, Dict("nonlin_Bolt_num"=> nonlin_Bolt_num))
         # Median needed since errors shoot up when derivatieve
         # crosses zero
         @test median(nonlin_EisHu_autodiff./nonlin_EisHu_num.-1) < 0.1
-        #@test median(nonlin_emul_autodiff./nonlin_emul_num.-1) < 0.1
-        #@test median(nonlin_Bolt_autodiff./nonlin_Bolt_num.-1) < 0.1
     end
-    
-
 
     @testset "AreClsDiff" begin
         
