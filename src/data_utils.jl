b499fed448c9a558d4438e66a039d41004c66ce3
jaimerzp <jaimerz011235813@gmal.com>
2024-05-14 11:49:51 +0100

    remove extra code

--- src/data_utils.jl
+++ src/data_utils.jl
@@ -13,19 +13,6 @@ function _get_type(sacc_file, tracer_name)
     return pyconvert(String, typ)
 end
 
-function _get_cl_name(t1, t2)
-    name_dict = Dict(
-        ["lens", "lens"]=>"galaxy_density_cl",
-        ["lens", "source"]=>"galaxy_shearDensity_cl_e",
-        ["source", "lens"]=>"galaxy_shearDensity_cl_e",
-        ["source", "source"]=>"galaxy_shear_cl_ee")
-    name_1 = pyconvert(String, t1)[1:end-2]
-    name_2 = pyconvert(String, t2)[1:end-2]
-    name = [name_1, name_2]
-    cl_name = name_dict[name]
-    return cl_name 
-end
-
 function _apply_scale_cuts!(s, yaml_file)
     indices = Vector{Int}([])
     for cl in yaml_file["order"]
@@ -149,72 +136,3 @@ function make_data(sacc_file, yaml_file; kwargs...)
     
     return instructions, files
 end
-
-function make_data(s; kwargs...)
-
-    kwargs=Dict(kwargs)
-    kwargs_keys = [string(i) for i in collect(keys(kwargs))]
-        
-    #build quantities of interest
-    cls = Vector{Float64}([])
-    ls = []
-    indices = Vector{Int}([])
-    pairs = []
-    for cl in s.get_tracer_combinations()
-        t1, t2 = cl
-        cl_name = _get_cl_name(t1, t2)
-        l, c_ell, ind = s.get_ell_cl(cl_name, string(t1), string(t2),
-                                     return_cov=false, return_ind=true)
-        append!(indices, pyconvert(Vector{Int}, ind))
-        append!(cls, pyconvert(Vector{Float64}, c_ell))
-        push!(ls, pyconvert(Vector{Float64}, l))
-        push!(pairs, pyconvert(Vector{String}, [t1, t2]))
-    end
-    
-    names = unique(vcat(pairs...))
-    cov = pyconvert(Vector{Vector{Float64}}, s.covariance.dense)
-    cov = permutedims(hcat(cov...))[indices.+1, :][:, indices.+1]
-    w, v = eigen(cov)
-    cov = v * (diagm(abs.(w)) * v')
-    cov = tril(cov) + triu(cov', 1)
-    cov = Hermitian(cov)
-    inv_cov = inv(cov)
-    lengths = [length(l) for l in ls]
-    lengths = vcat([0], lengths)
-    idx  = cumsum(lengths)
-    types = [_get_type(s, name) for name in names]
-    
-    # build struct
-    instructions = Instructions(names, pairs, types, idx,
-                                cls, cov, inv_cov)
-    
-    # Initialize
-    files = Dict{String}{Vector}()
-    # Load in l's
-    for (pair, l) in zip(pairs, ls)
-        t1, t2 = pair
-        println(t1, " ", t2, " ", length(l))
-        merge!(files, Dict(string("ls_", t1, "_", t2)=> l))
-    end
-    
-    # Load in nz's
-    for (name, tracer) in s.tracers.items()
-        if string(name) in names
-            if string("nz_", name) in kwargs_keys
-                println(string("using custom nz for ", string("nz_", name)))
-                nzs = kwargs[Symbol("nz_", name)]
-                z= pyconvert(Vector{Float64}, nzs["z"])
-                nz=pyconvert(Vector{Float64}, nzs["dndz"])
-                merge!(files, Dict(string("nz_", name)=>[z, nz]))
-            else
-                if string(tracer.quantity) != "cmb_convergence"
-                    z=pyconvert(Vector{Float64}, tracer.z)
-                    nz=pyconvert(Vector{Float64}, tracer.nz)
-                    merge!(files, Dict(string("nz_", name)=>[z, nz]))
-                end
-            end
-        end
-    end
-    
-    return instructions, files
-end 
