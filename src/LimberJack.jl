59a77728df1fde9da4cba99225042800517a5386
jaimerzp <jaimerz011235813@gmail.com>
2025-09-05 11:11:12 +0100

    bug

--- src/LimberJack.jl
+++ src/LimberJack.jl
@@ -10,7 +10,7 @@ export Theory
 export make_data
 
 using Interpolations, LinearAlgebra, Statistics, QuadGK
-using NPZ, NumericalIntegration, PythonCall, Artifacts 
+using NPZ, NumericalIntegration, Artifacts 
 include("core.jl")
 include("boltzmann.jl")
 include("data_utils.jl")
